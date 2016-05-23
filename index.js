08acff6d06f6fca64f16d070435255588b41e085
Andrea Carraro <andrea.carraro@mosaicoon.com>
2016-05-23 14:56:42 +0200

    Comment private methods

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = postcss.plugin('postcss-nested-ancestors', function (opts) {
 
     var parentsStack = [],
         placeholderRegex = new RegExp(
-            /*
+            /**
              * Get all ancestors placeholder recurrencies:
              * ^&, ^^&, ^^^&, [...]
              */
@@ -24,18 +24,12 @@ module.exports = postcss.plugin('postcss-nested-ancestors', function (opts) {
             'g'
         );
 
-    /*
-     * getParentSelectorAtLevel()
+    /**
+     * Walk current ancestor stack and
+     * assembly ancestor selector at the given depth.
      *
-     * Assembly current ancestor selector.
-     *
-     * nestingLevel param allows to select
-     * an higher element up into ancestors hierarchy
-     *
-     * eg.
-     * nestingLevel = 0 is direct parent (same of "&")
-     * nestingLevel = 1 is grandparent
-     * ...
+     * @param  {Number} ancestor nesting depth ( 0 = &, 1 = grandparent, ...)
+     * @return {String} ancestor selector
      */
     function getParentSelectorAtLevel(nestingLevel) {
         nestingLevel = nestingLevel || 1;
@@ -49,13 +43,27 @@ module.exports = postcss.plugin('postcss-nested-ancestors', function (opts) {
             .replace(/\s&/g, '');   // Remove empty spaces before "&"
     }
 
+    /**
+     * Given an ancestor placeholder,
+     * returns the corresponding selector fragment.
+     * Used as replacer in .replace method
+     *
+     * @param  {String} placeholder eg.^^&
+     * @return {String} string      ancestor selector fragment
+     */
     function placeholderReplacer(placeholder) {
         return getParentSelectorAtLevel(
-            // Get how many levels ("^") has current placeholder
+            // Get how many level symbols ("^") has current placeholder
             placeholder.split(opts.levelSymbol).length - 1
         );
     }
 
+    /**
+     * Replace any ancestor placeholder into a given selector.
+     *
+     * @param  {String} selector
+     * @return {String} selector
+     */
     function replacePlaceholders(selector) {
         // Find placeholders and replace them with matching parent selector
         return selector.replace(
@@ -74,8 +82,10 @@ module.exports = postcss.plugin('postcss-nested-ancestors', function (opts) {
                     parentsStack.push(rule.parent.selector);
                 }
 
-                // Replace parents placeholders in rule selctor
+                // Replace parents placeholders in rule selector
                 rule.selectors = rule.selectors.map(replacePlaceholders);
+
+                // Process child rules
                 process(rule);
             }
 
@@ -85,5 +95,3 @@ module.exports = postcss.plugin('postcss-nested-ancestors', function (opts) {
     };
     return process;
 });
-
-
