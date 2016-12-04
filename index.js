9233806c6a380360528af46e6659561ef8e83513
Nathan Hood <nathanhood@me.com>
2016-12-03 21:18:22 -0600

    Refactor code/remove ‘replaceDeclarations’ option

--- index.js
+++ index.js
@@ -1,100 +1,80 @@
 var postcss = require('postcss'),
-	assign = require('object-assign'),
 	escRgx = require('escape-string-regexp');
 
-module.exports = postcss.plugin('postcss-nested-ancestors', function (opts) {
-	opts = assign({
-		placeholder: '^&',
-		replaceDeclarations: false
-	}, opts);
-
-	// Advanced options
-	opts = assign({
-		levelSymbol: opts.levelSymbol || opts.placeholder.charAt(0),
-		parentSymbol: opts.parentSymbol || opts.placeholder.charAt(1)
+module.exports = postcss.plugin('postcss-nested-ancestors', opts => {
+	opts = Object.assign({
+		levelSymbol: opts.levelSymbol || '^',
+		parentSymbol: opts.parentSymbol || '&',
+		placeholder: '^&'
 	}, opts);
 
 	var parentStack = [],
-
-		// Get all ancestors placeholder recurrencies: ^&, ^^&, ^^^&, [...]
 		placeholderRegex = new RegExp(
-			// eslint-disable-next-line max-len
 			'([' + escRgx(opts.levelSymbol) + ']+)(' + escRgx(opts.parentSymbol) + ')(.*)',
 			'g'
 		),
-
-		// Get any space preceding an ampersand
 		spacesAndAmpersandRegex = /\s&/g;
 
 	/**
-	 * Walk current ancestor stack and
-	 * assembly ancestor selector at the given depth.
+	 * Determine how many level symbols ('^') exist
 	 *
-	 * @param  {Number} ancestor nesting depth (0 = &, 1 = ^& = grandparent...)
-	 * @param  {Object} a PostCSS Rule object
-	 * @param  {Object} a PostCSS Result object
-	 * @return {String} ancestor selector
+	 * @param {string} levelSymbol
+	 * @returns {number}
 	 */
-	function getParentSelectorAtLevel(nestingLevel, rule, result) {
-		nestingLevel = nestingLevel || 1;
+	function getNestingLevel(levelSymbol) {
+		return levelSymbol.split(opts.levelSymbol).length - 1 || 1
+	}
+
+	/**
+	 * Find parent selector from corresponding level symbol
+	 *
+	 * @param  {number} levelSymbol - Ancestor nesting depth (0 = &, 1 = ^&)
+	 * @param  {object} rule - PostCSS Rule object
+	 * @param  {object} result - PostCSS Result object
+	 * @return {string}
+	 */
+	function getParentSelector(levelSymbol, rule, result) {
+		let nestingLevel = getNestingLevel(levelSymbol);
 
 		// Set a warning when nestingLevel >= parentStack.length
 		if ( nestingLevel >= parentStack.length ) {
 			rule.warn(result, 'Parent selector exceeds current stack.');
 		}
 
-		// Create an array of matching parent selectors
-		return parentStack.filter( function (selector, index) {
-			return index < parentStack.length - nestingLevel;
-		})
+		return parentStack.filter((selector, index) => {
+				return index < parentStack.length - nestingLevel;
+			})
 			.join(' ')
-			.replace(spacesAndAmpersandRegex, '');  // Remove " &"
+			.replace(spacesAndAmpersandRegex, '');
 	}
 
 	/**
-	 * Given an ancestor placeholder,
-	 * returns the corresponding selector fragment.
-	 * Used as replacer in .replace method
+	 * Replace any ancestor placeholder into given selector(s)/string.
 	 *
-	 * @param  {String} levelSymbol (eg.^^)
-	 * @param  {Object} a PostCSS Rule object
-	 * @param  {Object} a PostCSS Result object
-	 * @return {String} string      ancestor selector fragment
-	 */
-	function placeholderReplacer(levelSymbol, rule, result) {
-		return getParentSelectorAtLevel(
-
-			// Determine how many level symbols ("^") there are
-			levelSymbol.split(opts.levelSymbol).length - 1,
-			rule,
-			result
-		);
-	}
-
-	/**
-	 * Replace any ancestor placeholder into a given selector/string.
-	 *
-	 * @param  {String} CSS selector / string
-	 * @param  {Object} a PostCSS Rule object
-	 * @param  {Object} a PostCSS Result object
-	 * @return {String} selector
+	 * @param  {string} selector - CSS selector
+	 * @param  {object} rule - PostCSS Rule object
+	 * @param  {object} result - PostCSS Result object
+	 * @return {string}
 	 */
 	function replacePlaceholders(selector, rule, result) {
-		return selector.split(/,\s*/).map(function (sel) {
-			return sel.replace(
-				placeholderRegex,
-				function (m, levelSymbol, parentSymbol, end) {
-					return placeholderReplacer(levelSymbol, rule, result)
-						.split(/,\s*/)
-						.map(function (parent) {
-							return parent + end;
-						}).join(', ');
-				}
-			);
-		}).join(', ');
+		return selector.split(/,\s*/)
+			.map(sel => {
+				return sel.replace(
+					placeholderRegex,
+					(m, levelSymbol, parentSymbol, end) => {
+						return getParentSelector(levelSymbol, rule, result)
+							.split(/,\s*/)
+							.map(parent => {
+								return parent + end;
+							})
+							.join(', ');
+					}
+				);
+			})
+			.join(', ');
 	}
 
-	var process = function (node, result) {
+	function process(node, result) {
 		node.walkRules(rule => {
 			// Replace parent placeholders in rule selectors
 			rule.selector = replacePlaceholders(rule.selector, rule, result);
@@ -110,7 +90,7 @@ module.exports = postcss.plugin('postcss-nested-ancestors', function (opts) {
 		parentStack.pop();
 	};
 
-	return function (root, result) {
+	return (root, result) => {
 		process(root, result);
 	};
 });
