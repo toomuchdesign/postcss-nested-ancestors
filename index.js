14f6b2093f37452215e3af53896d2e74121d8fee
Nathan Hood <nathanhood@me.com>
2016-12-03 20:38:51 -0600

    Convert spaces to tabs/Minor refactor

--- index.js
+++ index.js
@@ -1,126 +1,116 @@
 var postcss = require('postcss'),
-    assign = require('object-assign'),
-    escRgx = require('escape-string-regexp');
+	assign = require('object-assign'),
+	escRgx = require('escape-string-regexp');
 
 module.exports = postcss.plugin('postcss-nested-ancestors', function (opts) {
-    opts = assign({
-        placeholder: '^&',
-        replaceDeclarations: false
-    }, opts);
+	opts = assign({
+		placeholder: '^&',
+		replaceDeclarations: false
+	}, opts);
 
-    // Advanced options
-    opts = assign({
-        levelSymbol: opts.levelSymbol || opts.placeholder.charAt(0),
-        parentSymbol: opts.parentSymbol || opts.placeholder.charAt(1)
-    }, opts);
+	// Advanced options
+	opts = assign({
+		levelSymbol: opts.levelSymbol || opts.placeholder.charAt(0),
+		parentSymbol: opts.parentSymbol || opts.placeholder.charAt(1)
+	}, opts);
 
-    var parentsStack = [],
+	var parentStack = [],
 
-        // Get all ancestors placeholder recurrencies: ^&, ^^&, ^^^&, [...]
-        placeholderRegex = new RegExp(
-            // eslint-disable-next-line max-len
-            '([' + escRgx(opts.levelSymbol) + ']+)(' + escRgx(opts.parentSymbol) + ')(.*)',
-            'g'
-        ),
+		// Get all ancestors placeholder recurrencies: ^&, ^^&, ^^^&, [...]
+		placeholderRegex = new RegExp(
+			// eslint-disable-next-line max-len
+			'([' + escRgx(opts.levelSymbol) + ']+)(' + escRgx(opts.parentSymbol) + ')(.*)',
+			'g'
+		),
 
-        // Get any space preceding an ampersand
-        spacesAndAmpersandRegex = /\s&/g;
+		// Get any space preceding an ampersand
+		spacesAndAmpersandRegex = /\s&/g;
 
-    /**
-     * Walk current ancestor stack and
-     * assembly ancestor selector at the given depth.
-     *
-     * @param  {Number} ancestor nesting depth (0 = &, 1 = ^& = grandparent...)
-     * @param  {Object} a PostCSS Rule object
-     * @param  {Object} a PostCSS Result object
-     * @return {String} ancestor selector
-     */
-    function getParentSelectorAtLevel(nestingLevel, rule, result) {
-        nestingLevel = nestingLevel || 1;
+	/**
+	 * Walk current ancestor stack and
+	 * assembly ancestor selector at the given depth.
+	 *
+	 * @param  {Number} ancestor nesting depth (0 = &, 1 = ^& = grandparent...)
+	 * @param  {Object} a PostCSS Rule object
+	 * @param  {Object} a PostCSS Result object
+	 * @return {String} ancestor selector
+	 */
+	function getParentSelectorAtLevel(nestingLevel, rule, result) {
+		nestingLevel = nestingLevel || 1;
 
-        // Set a warning when nestingLevel >= parentsStack.length
-        if ( nestingLevel >= parentsStack.length ) {
-            rule.warn(result, 'Parent selector exceeds current stack.');
-        }
+		// Set a warning when nestingLevel >= parentStack.length
+		if ( nestingLevel >= parentStack.length ) {
+			rule.warn(result, 'Parent selector exceeds current stack.');
+		}
 
-        // Create an array of matching parent selectors
-        return parentsStack.filter( function (selector, index) {
-            return index < parentsStack.length - nestingLevel;
-        })
-            .join(' ')
-            .replace(spacesAndAmpersandRegex, '');  // Remove " &"
-    }
+		// Create an array of matching parent selectors
+		return parentStack.filter( function (selector, index) {
+			return index < parentStack.length - nestingLevel;
+		})
+			.join(' ')
+			.replace(spacesAndAmpersandRegex, '');  // Remove " &"
+	}
 
-    /**
-     * Given an ancestor placeholder,
-     * returns the corresponding selector fragment.
-     * Used as replacer in .replace method
-     *
-     * @param  {String} levelSymbol (eg.^^)
-     * @param  {Object} a PostCSS Rule object
-     * @param  {Object} a PostCSS Result object
-     * @return {String} string      ancestor selector fragment
-     */
-    function placeholderReplacer(levelSymbol, rule, result) {
-        return getParentSelectorAtLevel(
+	/**
+	 * Given an ancestor placeholder,
+	 * returns the corresponding selector fragment.
+	 * Used as replacer in .replace method
+	 *
+	 * @param  {String} levelSymbol (eg.^^)
+	 * @param  {Object} a PostCSS Rule object
+	 * @param  {Object} a PostCSS Result object
+	 * @return {String} string      ancestor selector fragment
+	 */
+	function placeholderReplacer(levelSymbol, rule, result) {
+		return getParentSelectorAtLevel(
 
-            // Determine how many level symbols ("^") there are
-            levelSymbol.split(opts.levelSymbol).length - 1,
-            rule,
-            result
-        );
-    }
+			// Determine how many level symbols ("^") there are
+			levelSymbol.split(opts.levelSymbol).length - 1,
+			rule,
+			result
+		);
+	}
 
-    /**
-     * Replace any ancestor placeholder into a given selector/string.
-     *
-     * @param  {String} CSS selector / string
-     * @param  {Object} a PostCSS Rule object
-     * @param  {Object} a PostCSS Result object
-     * @return {String} selector
-     */
-    function replacePlaceholders(selector, rule, result) {
-        return selector.split(/,\s*/).map(function (sel) {
-            return sel.replace(
-                placeholderRegex,
-                function (m, levelSymbol, parentSymbol, end) {
-                    return placeholderReplacer(levelSymbol, rule, result)
-                        .split(/,\s*/)
-                        .map(function (parent) {
-                            return parent + end;
-                        }).join(', ');
-                }
-            );
-        }).join(', ');
-    }
+	/**
+	 * Replace any ancestor placeholder into a given selector/string.
+	 *
+	 * @param  {String} CSS selector / string
+	 * @param  {Object} a PostCSS Rule object
+	 * @param  {Object} a PostCSS Result object
+	 * @return {String} selector
+	 */
+	function replacePlaceholders(selector, rule, result) {
+		return selector.split(/,\s*/).map(function (sel) {
+			return sel.replace(
+				placeholderRegex,
+				function (m, levelSymbol, parentSymbol, end) {
+					return placeholderReplacer(levelSymbol, rule, result)
+						.split(/,\s*/)
+						.map(function (parent) {
+							return parent + end;
+						}).join(', ');
+				}
+			);
+		}).join(', ');
+	}
 
-    var process = function (node, result) {
-        node.each( function (rule) {
-            if (rule.type === 'rule') {
+	var process = function (node, result) {
+		node.walkRules(rule => {
+			// Replace parent placeholders in rule selectors
+			rule.selector = replacePlaceholders(rule.selector, rule, result);
 
-                // Replace parents placeholders in rule selectors
-                rule.selector = replacePlaceholders(rule.selector, rule, result);
+			// Add selector to current parent stack
+			parentStack.push(rule.selector);
 
-                // Add current selector to current parent stack
-                parentsStack.push(rule.selector);
+			// Process child rules
+			process(rule, result);
+		});
 
-                // Process child rules
-                process(rule, result);
-            }
+		// Remove current parent stack item at the end of each child iteration
+		parentStack.pop();
+	};
 
-            // Optionally replace parents placeholders into declarations
-            // eslint-disable-next-line brace-style
-            else if (opts.replaceDeclarations && rule.type === 'decl') {
-                rule.value = replacePlaceholders(rule.value, rule, result);
-                rule.prop = replacePlaceholders(rule.prop, rule, result);
-            }
-        });
-
-        // Remove current parent stack item at the end of each child iteration
-        parentsStack.pop();
-    };
-
-    return function (root, result) {
-        process(root, result);
-    };
+	return function (root, result) {
+		process(root, result);
+	};
 });
--- test.js
+++ test.js
@@ -1,160 +1,144 @@
 import postcss from 'postcss';
-import test    from 'ava';
+import test from 'ava';
 
 import plugin from './';
 
 function run(t, input, output, opts = { }, warnings = 0) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then( result => {
-            t.deepEqual(result.css, output);
-            t.deepEqual(result.warnings().length, warnings);
-        });
+	return postcss([ plugin(opts) ]).process(input)
+		.then( result => {
+			t.deepEqual(result.css, output);
+			t.deepEqual(result.warnings().length, warnings);
+		});
 }
 
 test('Chain ancestor in a simple case', t => {
-    return run( t,
-                '.a{ &:hover{ ^&-b{} } }',
-                '.a{ &:hover{ .a-b{} } }',
-                { }
-    );
+	return run( t,
+		'.a{ &:hover{ ^&-b{} } }',
+		'.a{ &:hover{ .a-b{} } }',
+		{ }
+	);
 });
 
 test('Prepend ancestor in a simple case', t => {
-    return run( t,
-                '.a{ &:hover{ ^& .b{} } }',
-                '.a{ &:hover{ .a .b{} } }',
-                { }
-    );
+	return run( t,
+		'.a{ &:hover{ ^& .b{} } }',
+		'.a{ &:hover{ .a .b{} } }',
+		{ }
+	);
 });
 
 test('Chain 2 ancestors in double selector', t => {
-    return run( t,
-                '.a{ &:hover{ ^&-b, ^&-c{} } }',
-                '.a{ &:hover{ .a-b, .a-c{} } }',
-                { }
-    );
+	return run( t,
+		'.a{ &:hover{ ^&-b, ^&-c{} } }',
+		'.a{ &:hover{ .a-b, .a-c{} } }',
+		{ }
+	);
 });
 
 test('Prepend 2 ancestors in double selector', t => {
-    return run( t,
-                '.a{ &:hover{ ^& .b, ^& .c{} } }',
-                '.a{ &:hover{ .a .b, .a .c{} } }',
-                { }
-    );
+	return run( t,
+		'.a{ &:hover{ ^& .b, ^& .c{} } }',
+		'.a{ &:hover{ .a .b, .a .c{} } }',
+		{ }
+	);
 });
 
 test('Replace with root comment', t => {
-    return run( t,
-                '/* This is a comment */ .a{ &:hover{ ^& .b, ^& .c{} } }',
-                '/* This is a comment */ .a{ &:hover{ .a .b, .a .c{} } }',
-                { }
-    );
+	return run( t,
+		'/* This is a comment */ .a{ &:hover{ ^& .b, ^& .c{} } }',
+		'/* This is a comment */ .a{ &:hover{ .a .b, .a .c{} } }',
+		{ }
+	);
 });
 
 test('Replace with nested comment', t => {
-    return run( t,
-                '.a{ &:hover{ /* This is a comment */ ^& .b, ^& .c{} } }',
-                '.a{ &:hover{ /* This is a comment */ .a .b, .a .c{} } }',
-                { }
-    );
-});
-
-test('Replace declaration values if replaceValues is true', t => {
-    return run( t,
-                '.a{ &:hover { &:before { content: "^&"; } } }',
-                '.a{ &:hover { &:before { content: ".a:hover"; } } }',
-                { replaceDeclarations: true }
-    );
-});
-
-test('Replace declaration values if replaceDeclarations is true', t => {
-    return run( t,
-                '.a{ &:hover { &:before { content: "^^&"; } } }',
-                '.a{ &:hover { &:before { content: ".a"; } } }',
-                { replaceDeclarations: true }
-    );
+	return run( t,
+		'.a{ &:hover{ /* This is a comment */ ^& .b, ^& .c{} } }',
+		'.a{ &:hover{ /* This is a comment */ .a .b, .a .c{} } }',
+		{ }
+	);
 });
 
 test('Replace ancestors at different nesting levels', t => {
-    return run( t,
-                '.a{ &:hover{ ^&-b{} } .c{ .d{ ^&-e{} } } .z{} }',
-                '.a{ &:hover{ .a-b{} } .c{ .d{ .a .c-e{} } } .z{} }',
-                { }
-    );
+	return run( t,
+		'.a{ &:hover{ ^&-b{} } .c{ .d{ ^&-e{} } } .z{} }',
+		'.a{ &:hover{ .a-b{} } .c{ .d{ .a .c-e{} } } .z{} }',
+		{ }
+	);
 });
 
 test('Replace ancestors with 4 different hierarchy levels (1 exceeding)', t => {
-    return run( t,
-                '.a{ &-b{ &-c{ &-d{} ^&-d, ^&-d{} ^^&-d{} ^^^&-d{} } } }',
-                '.a{ &-b{ &-c{ &-d{} .a-b-d, .a-b-d{} .a-d{} -d{} } } }',
-                { },
-                1
-    );
+	return run( t,
+		'.a{ &-b{ &-c{ &-d{} ^&-d, ^&-d{} ^^&-d{} ^^^&-d{} } } }',
+		'.a{ &-b{ &-c{ &-d{} .a-b-d, .a-b-d{} .a-d{} -d{} } } }',
+		{ },
+		1
+	);
 });
 
 test('Process 2 nested ancestors', t => {
-    return run( t,
-                '.a{ &-b{ &-c{ ^^&-d{ &-e{ ^^^^&-f{ } } } } } }',
-                '.a{ &-b{ &-c{ .a-d{ &-e{ .a-f{ } } } } } }',
-                { }
-    );
+	return run( t,
+		'.a{ &-b{ &-c{ ^^&-d{ &-e{ ^^^^&-f{ } } } } } }',
+		'.a{ &-b{ &-c{ .a-d{ &-e{ .a-f{ } } } } } }',
+		{ }
+	);
 });
 
 test('Process nested ancestor close to > \, + and ~ selectors', t => {
-    return run( t,
-                '.a{ &-b{ > ^&-c{} + ^&-d{} ~ ^&-e{} } }',
-                '.a{ &-b{ > .a-c{} + .a-d{} ~ .a-e{} } }',
-                { }
-    );
+	return run( t,
+		'.a{ &-b{ > ^&-c{} + ^&-d{} ~ ^&-e{} } }',
+		'.a{ &-b{ > .a-c{} + .a-d{} ~ .a-e{} } }',
+		{ }
+	);
 });
 
 test('Return empty string when pointing to a non-existent ancestor', t => {
-    return run( t,
-                '.a{ &-b{ &-c{ ^^^^&-d{} } } }',
-                '.a{ &-b{ &-c{ -d{} } } }',
-                { },
-                1
-    );
+	return run( t,
+		'.a{ &-b{ &-c{ ^^^^&-d{} } } }',
+		'.a{ &-b{ &-c{ -d{} } } }',
+		{ },
+		1
+	);
 });
 
 test('Replace default ancestor selector with \"£%\"', t => {
-    return run( t,
-                '.a{ &-b{ &-c{ ££%-d{ £££%-f{ } } } } }',
-                '.a{ &-b{ &-c{ .a-d{ .a-f{ } } } } }',
-                { placeholder: '£%' }
-    );
+	return run( t,
+		'.a{ &-b{ &-c{ ££%-d{ £££%-f{ } } } } }',
+		'.a{ &-b{ &-c{ .a-d{ .a-f{ } } } } }',
+		{ placeholder: '£%' }
+	);
 });
 
 test('Replace default ancestor with custom levelSymbol and parentSymbol', t => {
-    return run( t,
-                '.a{ &-b{ &-c{ foofoobar-d{ foofoofoobar-f{ } } } } }',
-                '.a{ &-b{ &-c{ .a-d{ .a-f{ } } } } }',
-                { levelSymbol: 'foo', parentSymbol: 'bar' }
-    );
+	return run( t,
+		'.a{ &-b{ &-c{ foofoobar-d{ foofoofoobar-f{ } } } } }',
+		'.a{ &-b{ &-c{ .a-d{ .a-f{ } } } } }',
+		{ levelSymbol: 'foo', parentSymbol: 'bar' }
+	);
 });
 
 test('Complex nesting: ancestors with multiple selectors', t => {
-    return run( t,
-                '.a, .b{ &:after{ ^&-c{} } }',
-                '.a, .b{ &:after{ .a-c, .b-c{} } }',
-                { }
-    );
+	return run( t,
+		'.a, .b{ &:after{ ^&-c{} } }',
+		'.a, .b{ &:after{ .a-c, .b-c{} } }',
+		{ }
+	);
 });
 
 test('Complex nesting: ancestors with multiple selectors on newlines', t => {
-    return run( t,
-        `.a,
-        .b {
-            &:after {
-                ^&-c,
-                ^&-d {}
-            }
-        }`,
-        `.a, .b {
-            &:after {
-                .a-c, .b-c, .a-d, .b-d {}
-            }
-        }`,
-        { }
-    );
+	return run( t,
+		`.a,
+		.b {
+			&:after {
+				^&-c,
+				^&-d {}
+			}
+		}`,
+		`.a, .b {
+			&:after {
+				.a-c, .b-c, .a-d, .b-d {}
+			}
+		}`,
+		{ }
+	);
 });
